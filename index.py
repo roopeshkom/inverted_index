b2caa115ee02f6243d8f735fba76ef57a2d4947d
Roopeswar Kommalapati <roopeshkom@gmail.com>
2018-10-27 21:47:27 -0400

    Cleaned up code

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from os import listdir
 
+# Grabs words from files in given folder, and tracks their file and location in file
 def collect_words(file_dir):
     files = listdir(file_dir)
     words = []
@@ -7,15 +8,17 @@ def collect_words(file_dir):
     for file in files:
         with open(f'{file_dir}/{file}', 'r') as f:
             text = f.read()
-            words += list(map(lambda x: (x[1], file, x[0]), enumerate(text.split(' '))))
+            words += map(lambda x: (x[1], file, x[0]),
+                enumerate(text.split(' ')))
     
     return words
 
+# Takes a word and returns list of contiguous substrings in the word
 def get_contiguous_substrings(word):
-    return [word[start : start+len(word)+1-num] for num in range(1, 1+len(word))[::-1] for start in range(num)] 
+    return [word[start : start+len(word)+1-num]
+            for num in range(1, 1+len(word))[::-1]
+            for start in range(num)] 
 
 
-print(get_contiguous_substrings('hello'))
-
-#words = collect_words(file_dir='lorem_ipsum')
-#print(*words, sep='\n')
+words = collect_words(file_dir='lorem_ipsum')
+print(*words, sep='\n')
